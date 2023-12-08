041336ede99af6d00fd9aa3c4fd3df257ab848e9
greenwolf5 <25jazz25@gmail.com>
2023-12-08 00:34:04 -0500

    Added a lot
    Bot says who posted it
    Bot now transfers messages sent with links:
    Messages before the first link will appear before links
    messages after the first link (any messages after a second+ link will do this too) will be posted *after* the first message

--- DisBot.py
+++ DisBot.py
@@ -17,22 +17,38 @@ client = discord.Client(intents=intents)
 TWITTER = 'twitter'
 XCOM = 'x'
 def regexTwitterLinks(stringToRegex):
-    return re.findall(f"://(?:www.)?(?:{XCOM}|(?:{TWITTER}))\\.com/(.\\S*)", stringToRegex)
-def getFormattedMessage(message):
+    #([\s\S]*?)(?P<LinkChecker>(?:http(?:s)?://)+(?:www.)?(?:x|twitter)\.com/(.\S*))([\s\S]*?)(?=(?:(?P=LinkChecker))|$) 
+    #in theory this regex grabs all the floating text, and the after link message but I could not figure out how to get the groups to work in python
+    return re.findall(f"(?:http(?:s)?)?://(?:www.)?(?:{XCOM}|{TWITTER})\\.com/(.\\S*)", stringToRegex)
+def getFormattedMessage(message, author):
     #Regex to find if the message has either a twitter link or an x.com link the * means any character after the domain
-    completeMessage = ''
-    if (unformattedLinks := regexTwitterLinks(message.content)) != []:
-        #if discord lets you put one spoil embed in with multiple non spoiler embeds, change the function "removeSpoiledMesages" into "spoiledSpoiledMessages"
-        unformattedLinks = spoilSpoiledMessages(unformattedLinks, getSpoiledMessages(message))
-        for singleLink in unformattedLinks:
-            if(singleLink[len(singleLink)-1] != "|"):
-                completeMessage += 'https://fxtwitter.com/' + singleLink + '\n'
-            else:
-                completeMessage += '||https://fxtwitter.com/' + singleLink + '\n'
-        if(completeMessage == ''):
-            completeMessage = None
-        return completeMessage
+    completeMessage = (f'{author} posted\n')
+    freeMessages, unformattedLinks = regexFreeMessages(message.content)
+    if(freeMessages[0] != ''):
+        completeMessage += (f'{freeMessages[0]}')
+        if(freeMessages[0][len(freeMessages[0])-1] != '\n'):
+            completeMessage += '\n'
+    #if discord lets you put one spoil embed in with multiple non spoiler embeds, change the function "removeSpoiledMesages" into "spoiledSpoiledMessages"
+    unformattedLinks = spoilSpoiledMessages(unformattedLinks, getSpoiledMessages(message))
+    for singleLink in unformattedLinks:
+        if(singleLink[len(singleLink)-1] != "|"):
+            completeMessage += 'https://fxtwitter.com/' + singleLink + '\n'
+        else:
+            completeMessage += '||https://fxtwitter.com/' + singleLink + '\n'
+    if(completeMessage == ''):
+        completeMessage = None
+    return [completeMessage, freeMessages]
     
+def regexFreeMessages(stringToRegex):
+    match = re.findall(f"([\s\S]*?)(?:http(?:s)?://)+(?:www.)?(?:x|twitter)\.com/(.\S*)([\s\S]*?)(?=(?:http(?:s)?://)+(?:www.)?(?:x|twitter)\.com/.\S*|$)([\s\S]*?)", stringToRegex)
+    freeMessages = []
+    twitterLinks = []
+    for matchList in match:
+        freeMessages.append(matchList[0])
+        twitterLinks.append(matchList[1])
+        freeMessages.append(matchList[2])
+    return freeMessages, twitterLinks
+
     #awful bandaid fix
 def getSpoiledMessages(message):
     spoiled = re.findall('\\|\\|.*?\\|\\|', message.content) #Grabs *any* message in spoiler tags
@@ -58,6 +74,13 @@ def spoilSpoiledMessages(unformattedLinks, spoiled):
             unformattedLinks.insert(spoiledLinkIndex, (link+"||"))
     return unformattedLinks
 
+def makeMessage(freeMessages):
+    i = 1
+    completeFreeMessage = ''
+    for i in range(1,len(freeMessages)):
+        completeFreeMessage += freeMessages[i]
+    return completeFreeMessage
+
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
@@ -65,13 +88,15 @@ async def on_ready():
 @client.event
 async def on_message(message):
     #Check's to make sure the bot doesn't respond to itself
-    if message.author != client.user:   
-        completeMessage = getFormattedMessage(message)
-        if(completeMessage != None): #This is important b/c I remove messages from removedSpoiledMessages()
-            await asyncio.sleep(1) #Sleeps to help with the delay of when the picture embeds? :shrug:
-            await message.edit(suppress=True) #Removes the embeds from the original message b/c y'know it's ugly
-            await message.reply(completeMessage, allowed_mentions=discord.AllowedMentions.none(), silent = True) #Sends the message then, removes the mention so it doesn't @ the person
-
+    if message.author != client.user:
+        if(regexTwitterLinks(message.content) != []):
+            tupleCompleteAndFreeMessages = getFormattedMessage(message, message.author.display_name)
+            if(tupleCompleteAndFreeMessages[0] != None): #This is important b/c I remove messages from removedSpoiledMessages()
+                await asyncio.sleep(1) #Sleeps to help with the delay of when the picture embeds? :shrug:
+                await message.edit(suppress=True) #Removes the embeds from the original message b/c y'know it's ugly
+                await message.reply(tupleCompleteAndFreeMessages[0], allowed_mentions=discord.AllowedMentions.none(), silent = True) #Sends the message then, removes the mention so it doesn't @ the person
+                await message.channel.send(makeMessage(tupleCompleteAndFreeMessages[1]))
+            
 @client.event
 async def on_raw_message_delete(rawMessage):
     #Complicated, two cases incase the message is cached or not if it is cached it's wayyy more accurate, when not cached it probably doesn't work so will need to be fixed
@@ -102,7 +127,7 @@ async def on_raw_message_edit(rawMessage):
     if (regexTwitterLinks(editedMessage.content)) != []:
             async for postMessages in editedMessage.channel.history(limit = 2, after = editedMessage):
                 if postMessages.author == client.user:
-                    completeMessage = getFormattedMessage(editedMessage)
+                    completeMessage = getFormattedMessage(editedMessage, editedMessage.author.display_name)
                     if(completeMessage != None):
                         await postMessages.edit(content = completeMessage, allowed_mentions=discord.AllowedMentions.none())
                         
