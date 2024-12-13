dd86dd683f8f9269f86130f108beb7bab42a19af
greenwolf5 <25jazz25@gmail.com>
2024-12-12 23:29:18 -0500

    Lowercase s didn't work either

--- DisBot.py
+++ DisBot.py
@@ -37,7 +37,7 @@ id = discord.Object(serverId)
 @app_commands.allowed_installs(guilds=True, users=True)
 @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True) 
 async def embed(interaction: discord.Interaction, link: str): 
-    fullstring = "@silent " + interaction.user.display_name + " has posted\n"
+    fullstring = "@silent\n " + interaction.user.display_name + " has posted\n"
     fullstring += returnSingleLink(link)
     await interaction.response.send_message(fullstring)
 
@@ -46,7 +46,7 @@ async def embed(interaction: discord.Interaction, link: str):
 @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True) 
 @app_commands.guild_only()
 async def spoil(interaction: discord.Interaction, link: str): 
-    fullstring = "@silent " + interaction.user.display_name + " has posted\n"
+    fullstring = "@silent\n " + interaction.user.display_name + " has posted\n"
     fullstring += returnSpoiledSingleLink(link)
     await interaction.response.send_message(fullstring)
     
