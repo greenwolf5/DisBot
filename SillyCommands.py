e8b1fccac37273e4065d096e4d4f183f7fe0194f
Greenwolf5 <25jazz25@gmail.com>
2025-08-26 20:04:43 -0400

    Added Win and Candy, and a few for me

--- SillyCommands.py
+++ SillyCommands.py
@@ -17,11 +17,20 @@ listKnownUsers = [greenId, reshId, slyId, ponId, kodahnId, katId, jerbId, ashley
 keyWordDictionary = {}
 responseDictonary = {}
 
-keyWordDictionary[0] = ["jerby","Chaos","Skill issue",'uwu',"Shit"]
-responseDictonary[0] = [[False, jerbId, ["https://media.discordapp.net/attachments/1096302219990663289/1406668974611239082/image.png?ex=68a34e12&is=68a1fc92&hm=1de68b6091f812cc1a83bddfef4d6fc31a3c57cc4ac7f38c6ddbc5bad14e6170&=&format=webp&quality=lossless"]],[False,0,["https://tenor.com/bIkyP.gif"]],[True,0,"<:childeSkillIssue:939656128684498994>"],[False,0,["<a:6728_DiscordUwU:679524982493020168>","https://cdn.discordapp.com/attachments/1164308207733313586/1408269045534294038/image.png?ex=68a92040&is=68a7cec0&hm=13267cef467a10bc473a176be40d6ebdd11d91759102734c2bf0408686ea3455&","https://media.discordapp.net/attachments/1164308207733313586/1408271170054262794/image.png?ex=68a9223b&is=68a7d0bb&hm=28ad4b617043ea807ed9fdc25b239bb28405cfe2999eee89678b02b168260a95&=&format=webp&quality=lossless","https://media.discordapp.net/attachments/1164308207733313586/1408273374643359884/image.png?ex=68a92448&is=68a7d2c8&hm=3fb5d6c87db0ba7d85c524e0546c7f6624f06c3d7880ce3fc6142de89f918195&=&format=webp&quality=lossless","https://media.discordapp.net/attachments/1164308207733313586/1408279902981062858/image.png?ex=68a92a5d&is=68a7d8dd&hm=420cc7234c3b8b799a0c3e1f56f5f7e794a6cb353cf1f12453bd8c306f9e6783&=&format=webp&quality=lossless"]],[True,0,"<:letsfuckingshit:1221664187797868634>"]]
 
-keyWordDictionary[greenId] = ["bitch bot", "いただきます", '<a:SilvervaleGasms:1040264733737095299>',"<a:HUH:1049736597551194162>"]
-responseDictonary[greenId] = ["You fuckin' called?\n", "Fucking simp\n", [True, '<:WTF:637454072164646922>'],"<:uneedjesus:662380798279680013>"]
+uwuDict = ["<a:6728_DiscordUwU:679524982493020168>",
+           "https://cdn.discordapp.com/attachments/1164308207733313586/1408269045534294038/image.png?ex=68a92040&is=68a7cec0&hm=13267cef467a10bc473a176be40d6ebdd11d91759102734c2bf0408686ea3455&",
+           "https://media.discordapp.net/attachments/1164308207733313586/1408271170054262794/image.png?ex=68a9223b&is=68a7d0bb&hm=28ad4b617043ea807ed9fdc25b239bb28405cfe2999eee89678b02b168260a95&=&format=webp&quality=lossless",
+           "https://media.discordapp.net/attachments/1164308207733313586/1408273374643359884/image.png?ex=68a92448&is=68a7d2c8&hm=3fb5d6c87db0ba7d85c524e0546c7f6624f06c3d7880ce3fc6142de89f918195&=&format=webp&quality=lossless",
+           "https://media.discordapp.net/attachments/1164308207733313586/1408279902981062858/image.png?ex=68a92a5d&is=68a7d8dd&hm=420cc7234c3b8b799a0c3e1f56f5f7e794a6cb353cf1f12453bd8c306f9e6783&=&format=webp&quality=lossless",
+           "https://media.discordapp.net/attachments/635667155320569856/1410049332945621074/image.png?ex=68af9a46&is=68ae48c6&hm=3d8281a8c1e122f0238dc967d52dc4fde4c33eefec86c5f55ba53ece917f9265&=&format=webp&quality=lossless",
+           "https://media.discordapp.net/attachments/1164308207733313586/1410051672272539788/image.png?ex=68af9c74&is=68ae4af4&hm=f9901fe60d81bde11145a04cb6aea9efb3420edc194bf7345edcf11eab037cf0&=&format=webp&quality=lossless"]
+
+keyWordDictionary[0] = ["jerby","Chaos","Skill issue",'uwu',"Shit"]
+responseDictonary[0] = [[False, jerbId, ["https://media.discordapp.net/attachments/1096302219990663289/1406668974611239082/image.png?ex=68a34e12&is=68a1fc92&hm=1de68b6091f812cc1a83bddfef4d6fc31a3c57cc4ac7f38c6ddbc5bad14e6170&=&format=webp&quality=lossless"]],[False,0,["https://tenor.com/bIkyP.gif"]],[True,0,"<:childeSkillIssue:939656128684498994>"],[False,0,uwuDict],[True,0,"<:letsfuckingshit:1221664187797868634>"]]
+
+keyWordDictionary[greenId] = ["bitch bot", "いただきます", '<a:SilvervaleGasms:1040264733737095299>',"お前はゲーだよ","skill issueだよ","おっぱい","しょうがない","残念だ","ザーコ"]
+responseDictonary[greenId] = ["You fuckin' called?\n", "Fucking simp\n", [True, '<:WTF:637454072164646922>'],"He called you gay btw","(you have a skill issue)","Yeah I'm not translating that one","It can't be helped","What a shame","Noob"]
 
 keyWordDictionary[reshId] = ["hell yeah brother", "frfr", "on god", "no cap", "straight facts", "bruh", '<:SmugCat:784283846447202314>']
 responseDictonary[reshId] = ["hell yeah indeed brother\n", "frfr", "on god", "no cap", "straight facts", "bruh", [True, '<:SmugCat:784283846447202314>']]
